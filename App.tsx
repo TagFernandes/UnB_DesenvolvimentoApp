726226dcb6c22e2dde8b4addbf7500c5d0119b7b
Lucas Saad <lucassaadro@gmail.com>
2026-09-02 16:44:48 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
 
-export default function App() {
+export default function Login() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bem-vindo(a)!</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder='Email'
+        keyboardType='email-address'
+        autoCapitalize='none'
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder='Senha'
+        secureTextEntry
+      />
+
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +29,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 24,
+    backgroundColor: '#FEF8F8',
+    gap: 16
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    marginBottom: 24,
+    color: '#1A1A2E'
+  },
+  input: {
+    height: 48,
+    borderWidth: 1,
+    borderColor: '#D1d1d6',
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    fontSize: 16
+  },
+  button: {
+    height: 48,
+    backgroundColor: "#4f46e5",
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 8
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: '600'
   },
 });
